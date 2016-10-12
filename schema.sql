0c4323e8dc86545d07b6ac232c987bcbdf2de36b
William Lau <contact@cwlau.com>
2016-10-12 18:47:23 +0800

    Update sql and new event listener backend

--- schema.sql
+++ schema.sql
@@ -9,7 +9,18 @@ CREATE TABLE `contract_event` (
   `transaction_hash` varchar(90) DEFAULT NULL,
   `transaction_index` int(11) DEFAULT NULL,
   `event_name` varchar(45) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
   `datetime` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `music_event` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `block_id` VARCHAR(45) NOT NULL,
+  `contract_address` VARCHAR(80) NULL,
+  `user_address` VARCHAR(80) NULL,
+  `event_type` VARCHAR(45) NULL,
+  `description` TEXT NULL,
+  `datetime` DATETIME NULL,
+  PRIMARY KEY (`id`));
