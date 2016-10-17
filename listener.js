39aaa113072ccb28b60422d536527c98ba943c02
William CW Lau <contact@cwlau.com>
2016-10-17 12:10:22 +0800

    Adding contract info API (#5)
    * adding contract api
    
    * remove logging code

--- listener.js
+++ listener.js
@@ -143,8 +143,8 @@ var watchEvents = function(contract){
             msg += "\n    - MusicoinLogger: " + result.address + "";
             msg += "\n    - contract: " + result.args.sender + "";
 
-            console.dir(result);
-            console.log(msg);
+            // console.dir(result);
+            // console.log(msg);
 
             record_music_play(result.blockNumber, result.args.sender, result.args.plays.toNumber())
         }
@@ -156,8 +156,8 @@ var watchEvents = function(contract){
             msg += "\n    - MusicoinLogger: " + result.address + "";
             msg += "\n    - contract: " + result.args.sender + "";
 
-            console.dir(result);
-            console.log(msg);
+            // console.dir(result);
+            // console.log(msg);
 
             record_license(result.blockNumber, result.args.sender, result.args.version)
         }
@@ -211,10 +211,39 @@ var watchEvents = function(contract){
     });
 
 
+};
 
 
+app.get('/contract', function(req, res){
+    console.log('GET /contract')
+    contract_address = req.query.address;
+
+    var pppAbi = [{"constant":true,"inputs":[],"name":"resourceUrl","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":false,"inputs":[{"name":"_distributeBalanceFirst","type":"bool"}],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"metadata","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[],"name":"totalShares","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[],"name":"collectPendingPayment","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"licenseVersion","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"metadataVersion","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"coinsPerPlay","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"shares","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"totalEarned","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[],"name":"distributeBalance","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"newMetadata","type":"string"}],"name":"updateMetadata","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"newResourceUrl","type":"string"}],"name":"updateResourceUrl","outputs":[],"type":"function"},{"constant":false,"inputs":[],"name":"play","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"playCount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"contractVersion","outputs":[{"name":"","type":"string"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"pendingPayment","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_coinsPerPlay","type":"uint256"},{"name":"_recipients","type":"address[]"},{"name":"_shares","type":"uint256[]"}],"name":"updateLicense","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"recipients","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"type":"function"},{"inputs":[{"name":"_coinsPerPlay","type":"uint256"},{"name":"_resourceUrl","type":"string"},{"name":"_metadata","type":"string"},{"name":"_recipients","type":"address[]"},{"name":"_shares","type":"uint256[]"}],"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"plays","type":"uint256"}],"name":"playEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"version","type":"uint256"}],"name":"licenseUpdateEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"oldOwner","type":"address"},{"indexed":false,"name":"newOwner","type":"address"}],"name":"transferEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"oldResource","type":"string"},{"indexed":false,"name":"newResource","type":"string"}],"name":"resourceUpdateEvent","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"oldResource","type":"string"},{"indexed":false,"name":"newResource","type":"string"}],"name":"metadataUpdateEvent","type":"event"}]
+
+    var ppp = web3.eth.contract(pppAbi).at(contract_address);
+    var metadata = JSON.parse(ppp.metadata());
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({
+        "success": true,
+        "message": "",
+        "contract": {
+            "id": contract_address,
+            "resourceUrl": ppp.resourceUrl(),
+            "metadata": metadata,
+            "totalShares": ppp.totalShares(),
+            "licenseVersion": ppp.licenseVersion(),
+            "metadataVersion": ppp.metadataVersion(),
+            "coinsPerPlay": ppp.coinsPerPlay(),
+            "totalEarned": ppp.totalEarned(),
+            "owner": ppp.owner(),
+            "playCount": ppp.playCount()
+        }
+    }));
 
 };
+
+
 
 
 app.get('/', function(req, res){
